c0c13c3a150d790acfc47e18390f3d80a2d548f3
nicolas-tdc <nicolas.tachetdescombes@gmail.com>
2021-11-18 10:17:01 +0100

    Update admin.py

--- papernest/location_coverage/admin.py
+++ papernest/location_coverage/admin.py
@@ -1,7 +1,11 @@
+"""Admin settings for location coverage app"""
+
+
 from django.contrib import admin
 from .models import Provider, CoverageSite, CoverageType
 
 
+# Register models in admin.
 admin.site.register(Provider)
 admin.site.register(CoverageSite)
 admin.site.register(CoverageType)
